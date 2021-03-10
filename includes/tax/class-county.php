d183f51736d29f7b37ac3356bd84b55071122062
Paul Schreiber <paulschreiber@gmail.com>
2021-03-10 17:07:34 -0500

    feat: add fips and state to county taxonomy

--- includes/tax/class-county.php
+++ includes/tax/class-county.php
@@ -23,6 +23,21 @@ class County extends Taxonomy {
 	const SLUG = 'county';
 
 	/**
+	 * WordPress Hooks
+	 */
+	public static function hooks() {
+		parent::hooks();
+		add_action( self::TAX_SLUG . '_edit_form_fields', [ get_called_class(), 'edit_form_fields' ], 10, 2 );
+		add_action( self::TAX_SLUG . '_add_form_fields', [ get_called_class(), 'add_form_fields' ] );
+		add_action( 'created_' . self::TAX_SLUG, [ get_called_class(), 'save_form' ], 10, 2 );
+		add_action( 'edited_' . self::TAX_SLUG, [ get_called_class(), 'save_form' ], 10, 2 );
+
+		add_filter( 'manage_edit-' . self::TAX_SLUG . '_columns', [ get_called_class(), 'table_columns' ] );
+		add_filter( 'manage_' . self::TAX_SLUG . '_custom_column', [ get_called_class(), 'table_content' ], 10, 3 );
+		add_filter( 'manage_edit-' . self::TAX_SLUG . '_sortable_columns', [ get_called_class(), 'sortable_columns' ] );
+	}
+
+	/**
 	 * Register this taxonomy for profiles.
 	 */
 	public static function register_taxonomy() {
@@ -65,6 +80,128 @@ class County extends Taxonomy {
 		);
 	}
 
+	/**
+	 * Add state and fips columns to table. Rename 'Name' to 'County'.
+	 *
+	 * @param string[] $columns The column header labels keyed by column ID.
+	 */
+	public static function table_columns( $columns ) {
+		unset( $columns['description'] );
+		unset( $columns['slug'] );
+		unset( $columns['posts'] );
+		$columns['name']  = 'County';
+		$columns['state'] = 'State';
+		$columns['fips']  = 'FIPS';
+
+		return $columns;
+	}
+
+	/**
+	 * Add State and FIPS content to table.
+	 *
+	 * @param string $string      Blank string.
+	 * @param string $column_name Name of the column.
+	 * @param int    $term_id     Term ID.
+	 */
+	public static function table_content( $string, $column_name, $term_id ) {
+		if ( in_array( $column_name, [ 'state', 'fips' ], true ) ) {
+			return get_term_meta( $term_id, $column_name, true );
+		}
+	}
+
+	/**
+	 * Denote State and FIPS columns as sortable.
+	 *
+	 * @param array $sortable_columns An array of sortable columns.
+	 */
+	public static function sortable_columns( $sortable_columns ) {
+		$sortable_columns['state'] = 'State';
+		$sortable_columns['fips']  = 'FIPS';
+		return $sortable_columns;
+	}
+
+	/**
+	 * Save State and FIPS to the database.
+	 *
+	 * @param int $term_id Term ID.
+	 * @param int $tt_id   Term taxonomy ID.
+	 */
+	public static function save_form( $term_id, $tt_id ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		update_term_meta(
+			$term_id,
+			'fips',
+			filter_input( INPUT_POST, 'fips', FILTER_SANITIZE_NUMBER_INT )
+		);
+
+		$states = Helpers::states();
+		$state  = filter_input( INPUT_POST, 'state', FILTER_SANITIZE_STRING );
+		if ( isset( $states[ $state ] ) ) {
+			update_term_meta(
+				$term_id,
+				'state',
+				$state
+			);
+		} else {
+			delete_term_meta( $term_id, 'state' );
+		}
+	}
+
+	/**
+	 * Add state and FIPS fields to add form.
+	 *
+	 * @param string $taxonomy The taxonomy slug.
+	 */
+	public static function add_form_fields( $taxonomy ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		$states = Helpers::states();
+		?>
+		<div class="form-field state-wrap">
+			<label for="state">State</label>
+			<select id="state" name="state" ria-required="true">>
+				<option></option>
+			<?php foreach ( $states as $code => $name ) : ?>
+				<option value="<?php echo esc_attr( $code ); ?>"><?php echo esc_html( $name ); ?></option>
+			<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="form-field fips-wrap">
+			<label for="fips">FIPS Code</label>
+			<input type='number' id='fips' size='5' maxlength='5' name='fips' aria-required="true" />
+		</div>
+		<?php
+	}
+
+	/**
+	 * Add state and FIPS fields to edit form.
+	 *
+	 * @param WP_Term $term     Current taxonomy term object.
+	 * @param string  $taxonomy Current taxonomy slug.
+	 */
+	public static function edit_form_fields( $term, $taxonomy ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		$states = Helpers::states();
+		$state  = get_term_meta( $term->term_id, 'state', true );
+		$fips   = get_term_meta( $term->term_id, 'fips', true );
+
+		?>
+		<tr class="form-field form-required term-state-wrap">
+			<th scope="row"><label for="state">State</label></th>
+			<td>
+				<select id="state" name="state" ria-required="true">>
+					<option></option>
+				<?php foreach ( $states as $code => $name ) : ?>
+					<option <?php selected( $code, $state ); ?> value="<?php echo esc_attr( $code ); ?>"><?php echo esc_html( $name ); ?></option>
+				<?php endforeach; ?>
+				</select>
+
+			</td>
+		</tr>
+
+		<tr class="form-field form-required term-fips-wrap">
+			<th scope="row"><label for="fips">FIPS Code</label></th>
+			<td><input name="fips" id="fips" type="number" value="<?php echo esc_attr( $fips ); ?>" size="5" maxlength="5" aria-required="true"></td>
+		</tr>
+		<?php
+	}
 }
 
 add_action( 'after_setup_theme', [ '\Newspack\Govpack\County', 'hooks' ] );

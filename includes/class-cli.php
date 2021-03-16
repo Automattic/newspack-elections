a04f39a28a7277bba7bfca832d44dc1c7f6be373
Paul Schreiber <paulschreiber@gmail.com>
2021-03-15 22:59:01 -0400

    fix: fix namespace error for CLI::line()

--- includes/class-cli.php
+++ includes/class-cli.php
@@ -25,22 +25,22 @@ class CLI extends \WP_CLI_Command {
 	 */
 	public function seed_taxonomies( $args, $assoc_args ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
 		$count = \Newspack\Govpack\Tax\County::seed();
-		WP_CLI::line( "Inserted {$count} counties" );
+		\WP_CLI::line( "Inserted {$count} counties" );
 
 		$count = \Newspack\Govpack\Tax\Installation::seed();
-		WP_CLI::line( "Inserted {$count} installation methods" );
+		\WP_CLI::line( "Inserted {$count} installation methods" );
 
 		$count = \Newspack\Govpack\Tax\LegislativeBody::seed();
-		WP_CLI::line( "Inserted {$count} legislative bodies" );
+		\WP_CLI::line( "Inserted {$count} legislative bodies" );
 
 		$count = \Newspack\Govpack\Tax\OfficeHolderStatus::seed();
-		WP_CLI::line( "Inserted {$count} officeholder statuses" );
+		\WP_CLI::line( "Inserted {$count} officeholder statuses" );
 
 		$count = \Newspack\Govpack\Tax\Party::seed();
 		\WP_CLI::line( "Inserted {$count} parties." );
 
 		$count = \Newspack\Govpack\Tax\State::seed();
-		WP_CLI::line( "Inserted {$count} states" );
+		\WP_CLI::line( "Inserted {$count} states" );
 	}
 }
 

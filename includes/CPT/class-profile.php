742aac62949bd55ec88a1147c2ec12fc4521810b
Paul Schreiber <paulschreiber@gmail.com>
2021-03-25 19:24:58 -0400

    fix: profile: use constant for slug, not string

--- includes/CPT/class-profile.php
+++ includes/CPT/class-profile.php
@@ -113,7 +113,7 @@ class Profile {
 				'show_ui'      => true,
 				'supports'     => [ 'revisions', 'thumbnail' ],
 				'taxonomies'   => [ 'post_tag' ],
-				'as_taxonomy'  => 'govpack_profile_tax',
+				'as_taxonomy'  => \Newspack\Govpack\Tax\Profile::TAX_SLUG,
 				'menu_icon'    => 'dashicons-groups',
 				'rewrite'      => [
 					'slug'       => apply_filters( 'govpack_profile_filter_slug', 'profile' ),

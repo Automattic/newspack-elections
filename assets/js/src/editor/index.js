53a7c7a26f0cda5d20cc2bde95c08838a91e2457
Paul Schreiber <paulschreiber@gmail.com>
2021-03-28 15:19:39 -0400

    refactor: update profile block  to use v2 of the blocks API
    * add blockProps (needs to be on a <div>, not <ServerSideRender>)
    * make edit into a function (avoids react-hooks/rules-of-hooks error on
      useRef and useBlockProps)

--- assets/js/src/editor/index.js
+++ assets/js/src/editor/index.js
@@ -2,25 +2,67 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress/element';
 import { registerBlockType } from '@wordpress/blocks';
-import { InspectorControls } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { Panel, PanelBody, PanelRow, RadioControl } from '@wordpress/components';
+import { useRef } from '@wordpress/element';
 
 /*
  * import { ServerSideRender } from '@wordpress/editor'
  *    is deprecated.
  * Use
- *    import from @wordpress/server-side-render
- * instead. But it only has a default export, not a named export.
+ *    import ServerSideRender from @wordpress/server-side-render
+ * instead. But it only has a default export, not a named export,
+ * so you can't use braces.
  */
 import ServerSideRender from '@wordpress/server-side-render';
 
 import ProfileSelector from './components/profile-selector';
 
-/* with apiVersion 2, the block is not selectable in the editor */
+/**
+ * @param {Object} props The compoent properties.
+ */
+function Edit( props ) {
+	/**
+	 * @param {string} value The selected format.
+	 */
+	function updateFormat( value ) {
+		props.setAttributes( { format: value } );
+	}
+
+	const ref = useRef();
+	const blockProps = useBlockProps( { ref } );
+
+	return (
+		<div { ...blockProps }>
+			<InspectorControls>
+				<Panel>
+					<PanelBody title={ __( 'Govpack Profile', 'govpack' ) }>
+						<PanelRow>
+							<ProfileSelector props={ props } />
+						</PanelRow>
+						<PanelRow>
+							<RadioControl
+								label="Format"
+								selected={ props.attributes.format }
+								options={ [
+									{ value: 'full', label: 'Full' },
+									{ value: 'mini', label: 'Mini' },
+									{ value: 'wiki', label: 'Wiki' },
+								] }
+								onChange={ updateFormat }
+							/>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+			<ServerSideRender block="govpack/profile" attributes={ props.attributes } />
+		</div>
+	);
+}
+
 registerBlockType( 'govpack/profile', {
-	apiVersion: 1,
+	apiVersion: 2,
 	title: 'Govpack',
 	icon: 'groups',
 	category: 'embed',
@@ -36,41 +78,7 @@ registerBlockType( 'govpack/profile', {
 		},
 	},
 
-	edit: props => {
-		/**
-		 * @param {string} value The selected format.
-		 */
-		function updateFormat( value ) {
-			props.setAttributes( { format: value } );
-		}
-
-		return (
-			<Fragment>
-				<InspectorControls>
-					<Panel>
-						<PanelBody title={ __( 'Govpack Profile', 'govpack' ) }>
-							<PanelRow>
-								<ProfileSelector props={ props } />
-							</PanelRow>
-							<PanelRow>
-								<RadioControl
-									label="Format"
-									selected={ props.attributes.format }
-									options={ [
-										{ value: 'full', label: 'Full' },
-										{ value: 'mini', label: 'Mini' },
-										{ value: 'wiki', label: 'Wiki' },
-									] }
-									onChange={ updateFormat }
-								/>
-							</PanelRow>
-						</PanelBody>
-					</Panel>
-				</InspectorControls>
-				<ServerSideRender block="govpack/profile" attributes={ props.attributes } />
-			</Fragment>
-		);
-	},
+	edit: Edit,
 	save() {
 		return null;
 	},

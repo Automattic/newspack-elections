3b34082787275cdfba0ccede921d3a2f8bde68f6
Paul Schreiber <paulschreiber@gmail.com>
2021-03-28 12:25:57 -0400

    build: compile blocks

--- dist/editor.asset.php
+++ dist/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'fedd636d87a861f9a071538ddbefac1c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '98b1c1b3d8dbeff0ca640ebbc38fa88f');

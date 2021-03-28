57fc1a61dc64cd5d4c99241b19773712c02ed43a
Paul Schreiber <paulschreiber@gmail.com>
2021-03-27 21:52:04 -0400

    feat: add format radio buttons to profile block

--- assets/js/src/editor/index.js
+++ assets/js/src/editor/index.js
@@ -4,14 +4,14 @@
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { InspectorControls } from '@wordpress/block-editor';
-import { Panel, PanelBody, PanelRow } from '@wordpress/components';
+import { Panel, PanelBody, PanelRow, RadioControl } from '@wordpress/components';
 
 import ProfileSelector from './components/profile-selector';
 
 registerBlockType( 'newspack/govpack', {
 	apiVersion: 2,
 	title: 'Govpack',
-	icon: 'universal-access-alt',
+	icon: 'groups',
 	category: 'embed',
 	keywords: [ 'govpack' ],
 	attributes: {
@@ -19,15 +19,38 @@ registerBlockType( 'newspack/govpack', {
 			type: 'number',
 			default: 0,
 		},
+		format: {
+			type: 'string',
+			default: 'full',
+		},
 	},
 
 	edit: props => {
+		/**
+		 * @param {string} value The selected format.
+		 */
+		function updateFormat( value ) {
+			props.setAttributes( { format: value } );
+		}
+
 		return (
 			<InspectorControls>
 				<Panel>
 					<PanelBody title={ __( 'Govpack', 'govpack' ) }>
 						<PanelRow>
 							<ProfileSelector props={ props } />
+						</PanelRow>
+						<PanelRow>
+							<RadioControl
+								label="Format"
+								selected={ props.attributes.format }
+								options={ [
+									{ value: 'full', label: 'Full' },
+									{ value: 'mini', label: 'Mini' },
+									{ value: 'wiki', label: 'Wiki' },
+								] }
+								onChange={ updateFormat }
+							/>
 						</PanelRow>
 					</PanelBody>
 				</Panel>

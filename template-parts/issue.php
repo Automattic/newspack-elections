034ae6fd5e9f3b81b5ac273f6ce914176350e88d
Leander Iversen <leander@carpe-noctem.no>
2021-04-28 09:52:14 +0100

    fix: remove issue format options from template

--- template-parts/issue.php
+++ template-parts/issue.php
@@ -8,18 +8,16 @@
 // $issue_data is defined elsewhere.
 // phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 
-$classes = join( ' ', array_filter( [ 'govpack-profile', $atts['className'], $issue_data['format'] ] ) );
+$classes = join( ' ', array_filter( [ 'govpack-profile', $atts['className'] ] ) );
 ?>
 <aside class="<?php echo esc_attr( $classes ); ?>">
 	<p class="photo"><?php echo wp_kses_post( get_the_post_thumbnail( $issue_data['id'], 'govpack-square' ) ); ?></p>
 
 	<div class="name-details">
 		<h3 class="name"><?php echo esc_html( $issue_data['title'] ); ?></h3>
-
-		<?php if ( 'wiki' === $issue_data['format'] ) : ?>
 		<div class="biography">
 			<?php echo wp_kses_post( $issue_data['content'] ); ?>
 		</div>
-		<?php endif; ?>
 	</div>
+
 </aside>

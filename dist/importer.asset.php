5032722d77c186e59f0b7942264b4a4986bc095c
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2022-08-05 12:52:13 +0100

    working importer

--- dist/importer.asset.php
+++ dist/importer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '8822d429e61ba2d28adb');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'de6bca363b9b079bad6f');

afe51fcc08a3837b2c7844cd72c5c113601d717e
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2022-08-05 15:30:03 +0100

    made the action scheduker optional

--- includes/Importer/class-csv.php
+++ includes/Importer/class-csv.php
@@ -85,6 +85,7 @@ class CSV extends \Govpack\Core\Importer\Abstracts\Abstract_Importer {
 
 		update_option( 'govpack_import_group', self::import_group() );
 
+		$use_action_scheduler = true;
 
 		foreach ( $reader->getRecords() as $offset => $record ) {
 
@@ -93,10 +94,14 @@ class CSV extends \Govpack\Core\Importer\Abstracts\Abstract_Importer {
 				$record = array_merge( $record, $extra );
 			} 
 			
-			as_enqueue_async_action( 'govpack_import_csv_profile', [ 'data' => $record ], self::import_group() );
+			if($use_action_scheduler){
+				as_enqueue_async_action( 'govpack_import_csv_profile', [ 'data' => $record ], self::import_group() );
+			}
 		}
 
-		as_enqueue_async_action( 'govpack_import_cleanup', [], self::import_group() );
+		if($use_action_scheduler){
+			as_enqueue_async_action( 'govpack_import_cleanup', [], self::import_group() );
+		}
 
 	}
 

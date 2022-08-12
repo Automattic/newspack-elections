483750ebcbdedd7f6a47d23c1337b300b4348e88
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2022-08-12 17:45:17 +0100

    ran phpcbf

--- includes/Admin/Pages/class-import.php
+++ includes/Admin/Pages/class-import.php
@@ -25,9 +25,12 @@ class Import {
 		wp_enqueue_script( 'govpack-importer' );
 		wp_enqueue_style( 'wp-components' );
 		
-		wp_add_inline_script( "govpack-importer", "var govpack_importer_options = " . 
-			json_encode(["profiles_path" => admin_url('edit.php?post_type=' . Profile::CPT_SLUG )])
-		, 'before' );
+		wp_add_inline_script(
+			'govpack-importer',
+			'var govpack_importer_options = ' . 
+			json_encode( [ 'profiles_path' => admin_url( 'edit.php?post_type=' . Profile::CPT_SLUG ) ] ),
+			'before' 
+		);
 		
 
 		include __DIR__ . './../Views/import.php';

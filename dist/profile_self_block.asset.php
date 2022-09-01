2cc1f57c6ee23bbc08b66ab4fa0f243cbf7750f9
Paul Draper <paul@poweredbycoffee.co.uk>
2022-09-01 17:24:53 +0100

    Added Website in NormaliseProfile

--- dist/profile_self_block.asset.php
+++ dist/profile_self_block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a7768dbc6ecb6429d154');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4cb71ebe6938b8a5c8da');

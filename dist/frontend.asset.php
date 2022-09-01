09d51f848628a7b9610260a53ea70ef171e2deb6
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2022-09-01 18:36:16 +0100

    markdown readme

--- dist/frontend.asset.php
+++ dist/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '99ec3b91f32790113f1e');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a355c090ff075ab33b19');
--- dist/profile_self_block.asset.php
+++ dist/profile_self_block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4cb71ebe6938b8a5c8da');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2bb3774100a5ba95580a');

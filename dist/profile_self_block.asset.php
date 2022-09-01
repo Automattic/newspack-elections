8d9e01b953944894cc8cae6c3a4b661d842c1a78
Paul Draper <paul@poweredbycoffee.co.uk>
2022-09-01 17:14:54 +0100

    React mods to display comms if data available

--- dist/profile_self_block.asset.php
+++ dist/profile_self_block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5e2259eaed1dceecad37');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a7768dbc6ecb6429d154');

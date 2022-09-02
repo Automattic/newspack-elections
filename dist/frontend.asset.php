3c3fc89a656541547132605feeac61c7b7ca9f1b
Paul Draper <paul@poweredbycoffee.co.uk>
2022-09-02 16:27:11 +0100

    Profile SASS mods

--- dist/frontend.asset.php
+++ dist/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a355c090ff075ab33b19');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'd843c3bc20544431ba36');
--- dist/profile_self_block.asset.php
+++ dist/profile_self_block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '987411b0c96c7cb7dc7c');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '64f9bb0ac34ea43bedaa');

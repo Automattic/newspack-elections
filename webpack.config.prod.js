a8ebb6da0ed54d5208bda78d897250fc63643604
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2022-09-27 13:00:56 +0100

    removed the bundle analyser

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,10 +2,6 @@
  **** WARNING: No ES6 modules here. Not transpiled! ****
  */
 /* eslint-disable import/no-nodejs-modules */
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const getUpdatedWebpackConfig = require("./webpack.config.js")
-
 
 
 const getProductionConfig = function(env, arg){

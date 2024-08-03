99ad2eed930af58d376a95df2a2dbcb41fb4cc32
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-08-03 13:47:06 +0100

    removed unuses ids from Panels

--- src/editor/meta/Panels/Meta.jsx
+++ src/editor/meta/Panels/Meta.jsx
@@ -13,8 +13,9 @@ export const MetadataIdsPanel = (props) => {
             title="Metadata & IDS"
             name="gov-metadataids-communications"
         >
-			  <PanelRow>
-                <PanelTextControl meta={meta} label= "Govpack ID" meta_key="govpack_id" onChange={setPostMeta} placeholder="" />
+
+			<PanelRow>
+                <PanelTextControl meta={meta} label= "Ballotpedia ID" meta_key="balletpedia_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
 
 			<PanelRow>
@@ -22,19 +23,39 @@ export const MetadataIdsPanel = (props) => {
             </PanelRow>
 
 			<PanelRow>
-                <PanelTextControl meta={meta} label= "Bioguide ID" meta_key="usio_id" onChange={setPostMeta} placeholder="" />
+                <PanelTextControl meta={meta} label= "Google Entity ID" meta_key="google_entity_id" onChange={setPostMeta} placeholder="" />
+            </PanelRow>
+
+			<PanelRow>
+                <PanelTextControl meta={meta} label= "Govtrack ID" meta_key="govtrack_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
 
 			<PanelRow>
                 <PanelTextControl meta={meta} label= "Opensecrets ID" meta_key="opensecrets_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
-
+			
 			<PanelRow>
-                <PanelTextControl meta={meta} label= "District OCDID" meta_key="district_ocd_id" onChange={setPostMeta} placeholder="" />
+                <PanelTextControl meta={meta} label= "Open States ID" meta_key="openstates_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
 
 			<PanelRow>
-                <PanelTextControl meta={meta} label= "Open States ID" meta_key="openstates_id" onChange={setPostMeta} placeholder="" />
+                <PanelTextControl meta={meta} label= "VoteView" meta_key="icpsr_id" onChange={setPostMeta} placeholder="" />
+            </PanelRow>
+
+			<PanelRow>
+                <PanelTextControl meta={meta} label= "VoteSmart ID" meta_key="votesmart_id" onChange={setPostMeta} placeholder="" />
+            </PanelRow>
+
+			<PanelRow>
+                <PanelTextControl meta={meta} label= "Wikipedia ID" meta_key="wikipedia_id" onChange={setPostMeta} placeholder="" />
+            </PanelRow>
+
+		{/*	<PanelRow>
+                <PanelTextControl meta={meta} label= "Bioguide ID" meta_key="usio_id" onChange={setPostMeta} placeholder="" />
+            </PanelRow> 
+
+			<PanelRow>
+                <PanelTextControl meta={meta} label= "District OCDID" meta_key="district_ocd_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
 
 			<PanelRow>
@@ -50,39 +71,13 @@ export const MetadataIdsPanel = (props) => {
             </PanelRow>
 
 			<PanelRow>
-                <PanelTextControl meta={meta} label= "Govtrack ID" meta_key="govtrack_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
-                <PanelTextControl meta={meta} label= "VoteSmart ID" meta_key="votesmart_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
-                <PanelTextControl meta={meta} label= "Ballotpedia ID" meta_key="balletpedia_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
                 <PanelTextControl meta={meta} label= "Washington Post ID" meta_key="washington_post_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
-                <PanelTextControl meta={meta} label= "ICPSR ID" meta_key="icpsr_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
-                <PanelTextControl meta={meta} label= "Wikipedia ID" meta_key="wikipedia_id" onChange={setPostMeta} placeholder="" />
-            </PanelRow>
-
-			<PanelRow>
-                <PanelTextControl meta={meta} label= "Google Entity ID" meta_key="google_entity_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
 
 			<PanelRow>
                 <PanelTextControl meta={meta} label= "Candidate Committee ID" meta_key="committee_id" onChange={setPostMeta} placeholder="" />
             </PanelRow>
-
-			
-
+			*/}
 
 		</GovPackSidebarPanel>
 	)
